8b2aed37c1755a94b95d6c1575d0e226305af3c7
AlexeyShpavda <alexeyshpavda@gmail.com>
2019-04-28 08:45:29 +0300

    Build project structure

---
+++ FolderStructureExplorer.PL.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FolderStructureExplorer.PL.ConsoleApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
