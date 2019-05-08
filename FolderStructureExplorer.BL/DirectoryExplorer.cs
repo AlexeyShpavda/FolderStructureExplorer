723c770319d49633a49afe53df13868d7a519ddc
AlexeyShpavda <alexeyshpavda@gmail.com>
2019-05-08 16:10:16 +0300

    Create Events of finding directory / file

--- FolderStructureExplorer.BL/DirectoryExplorer.cs
+++ FolderStructureExplorer.BL/DirectoryExplorer.cs
@@ -2,12 +2,31 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using FolderStructureExplorer.BL.Contracts.EventsArgs;
 using FolderStructureExplorer.BL.Contracts.Interfaces;
 
 namespace FolderStructureExplorer.BL
 {
     public class DirectoryExplorer : IDirectoryExplorer
     {
+        #region Events
+        public event EventHandler<DirectoryEventArgs> NotifyAboutStartingOfExplore;
+
+        public event EventHandler<DirectoryEventArgs> NotifyAboutFinishingOfExplore;
+
+        public event EventHandler<FileEventArgs> NotifyThatFileWasFounded;
+
+        public event EventHandler<DirectoryEventArgs> NotifyThatDirectoryWasFounded;
+
+        public event EventHandler<FileEventArgs> NotifyThatFilePassedFilteringSuccessfully;
+
+        public event EventHandler<DirectoryEventArgs> NotifyThatDirectoryPassedFilteringSuccessfully;
+
+        public event EventHandler<FileEventArgs> NotifyThatFileNotFiltered;
+
+        public event EventHandler<DirectoryEventArgs> NotifyThatDirectoryNotFiltered;
+        #endregion
+
         public IEnumerable<string> Explore(string directoryPath)
         {
             var researchDirectories = new Queue<string>();
@@ -35,5 +54,47 @@ namespace FolderStructureExplorer.BL
                 }
             }
         }
+
+        #region OnEvents
+        protected virtual void OnNotifyAboutStartingOfExplore(DirectoryEventArgs e)
+        {
+            NotifyAboutStartingOfExplore?.Invoke(this, e);
+        }
+
+        protected virtual void OnNotifyAboutFinishingOfExplore(DirectoryEventArgs e)
+        {
+            NotifyAboutFinishingOfExplore?.Invoke(this, e);
+        }
+
+        protected virtual void OnNotifyThatFileWasFounded(FileEventArgs e)
+        {
+            NotifyThatFileWasFounded?.Invoke(this, e);
+        }
+
+        protected virtual void OnNotifyThatDirectoryWasFounded(DirectoryEventArgs e)
+        {
+            NotifyThatDirectoryWasFounded?.Invoke(this, e);
+        }
+
+        protected virtual void OnNotifyThatFilePassedFilteringSuccessfully(FileEventArgs e)
+        {
+            NotifyThatFilePassedFilteringSuccessfully?.Invoke(this, e);
+        }
+
+        protected virtual void OnNotifyThatDirectoryPassedFilteringSuccessfully(DirectoryEventArgs e)
+        {
+            NotifyThatDirectoryPassedFilteringSuccessfully?.Invoke(this, e);
+        }
+
+        protected virtual void OnNotifyThatFileNotFiltered(FileEventArgs e)
+        {
+            NotifyThatFileNotFiltered?.Invoke(this, e);
+        }
+
+        protected virtual void OnNotifyThatDirectoryNotFiltered(DirectoryEventArgs e)
+        {
+            NotifyThatDirectoryNotFiltered?.Invoke(this, e);
+        }
+        #endregion
     }
 }
