a785e8faf4017c38ceb2845b20eab20439a4ee50
AlexeyShpavda <alexeyshpavda@gmail.com>
2019-05-12 18:20:13 +0300

    Transfer FolderTreeCreator class to separate file

--- FolderStructureExplorer.BL.Tests/DirectoryExplorerTests.cs
+++ FolderStructureExplorer.BL.Tests/DirectoryExplorerTests.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Net.Mime;
-using System.Reflection;
-using FolderStructureExplorer.BL.Contracts.Interfaces;
+using FolderStructureExplorer.BL.Tests.Helpers;
 using NUnit.Framework;
 
 namespace FolderStructureExplorer.BL.Tests
@@ -27,7 +22,7 @@ namespace FolderStructureExplorer.BL.Tests
             }
 
             Directory.CreateDirectory(_testDirectoryPath);
-            var treeFolders = new TreeFolders(_testDirectoryPath);
+            var treeFolders = new FolderTreeCreator(_testDirectoryPath);
             treeFolders.Create(StructureDepth);
         }
 
@@ -61,41 +56,6 @@ namespace FolderStructureExplorer.BL.Tests
                 null, directoryInfo => directoryInfo.Name == folderName);
 
             Assert.AreEqual(directoryExplorer.Explore(_testDirectoryPath).Count(), expected);
-        }
-    }
-
-    internal class TreeFolders
-    {
-        private static Stack<string> _folders;
-
-        public TreeFolders(string path)
-        {
-            _folders = new Stack<string>();
-            _folders.Push(path);
-        }
-
-        public void Create(int numberFolders)
-        {
-            var extensionList = new List<string> {".txt", ".pdf", ".xlsm"};
-            if (numberFolders == 0)
-            {
-                return;
-            }
-
-            var folder = _folders.Pop();
-
-            for (var i = 1; i <= numberFolders; i++)
-            {
-                var path = $@"{folder}\{i}";
-
-                Directory.CreateDirectory(path);
-                foreach (var extension in extensionList)
-                {
-                    File.Create($@"{path}\{DateTime.Now:yyyy.MM.dd_HH-mm-ss}{extension}");
-                }
-                _folders.Push(path);
-                Create(numberFolders - 1);
-            }
         }
     }
 }
---
+++ FolderStructureExplorer.BL.Tests/Helpers/FolderTreeCreator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FolderStructureExplorer.BL.Tests.Helpers
+{
+    internal class FolderTreeCreator
+    {
+        private readonly Stack<string> _folders;
+
+        public FolderTreeCreator(string path)
+        {
+            _folders = new Stack<string>();
+            _folders.Push(path);
+        }
+
+        public void Create(int numberFolders)
+        {
+            var extensionList = new List<string> { ".txt", ".pdf", ".xlsm" };
+            if (numberFolders == 0)
+            {
+                return;
+            }
+
+            var folder = _folders.Pop();
+
+            for (var i = 1; i <= numberFolders; i++)
+            {
+                var path = $@"{folder}\{i}";
+
+                Directory.CreateDirectory(path);
+                foreach (var extension in extensionList)
+                {
+                    File.Create($@"{path}\{DateTime.Now:yyyy.MM.dd_HH-mm-ss}{extension}");
+                }
+                _folders.Push(path);
+                Create(numberFolders - 1);
+            }
+        }
+    }
+}
